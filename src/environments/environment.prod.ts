c853ef9bcdc3c9e8fa58f4c97fbeabbc759b568e
GhostDev <62150766+massndongo@users.noreply.github.com>
2024-08-20 15:00:49 +0000

    new build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  urlApi: 'https://technoweb.homeip.net/apis/sysinvent/nabysy/',
-  // urlApi: 'https://cloud-549579568.onetsolutions.network/app/web/nabysy/',
+  // urlApi: 'https://technoweb.homeip.net/apis/sysinvent/nabysy/',
+  urlApi: 'https://cloud-549579568.onetsolutions.network/app/web/nabysy/',
   production: true
 };
